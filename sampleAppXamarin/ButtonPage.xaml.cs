1c1bcf3acb48ae30d707ed410e63430482beaaf7
sabrinaj206 <sabrinajiang@Sabrinas-MacBook-Pro.local>
2017-08-10 15:57:33 -0700

    deleted uncessary stuff

---
+++ sampleAppXamarin/ButtonPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace sampleAppXamarin
+{
+    public partial class ButtonPage : ContentPage
+    {
+        public ButtonPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
