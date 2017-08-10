159504cf7296ea7b068b5da7062ec537347fb971
sabrinaj206 <sabrinajiang@Sabrinas-MacBook-Pro.local>
2017-08-10 15:06:09 -0700

    renamed without dash

--- iOS/Helpers/Settings.cs
+++ iOS/Helpers/Settings.cs
@@ -3,7 +3,7 @@
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 
-namespace sampleappxamarin.iOS.Helpers
+namespace sampleAppXamarin.iOS.Helpers
 {
   /// <summary>
   /// This is the Settings static class that can be used in your Core solution or in any
---
+++ sampleAppXamarin/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace sampleAppXamarin
+{
+    public partial class App : Application
+    {
+        public static bool UseMockDataStore = true;
+        public static string BackendUrl = "https://localhost:5000";
+
+        public static IDictionary<string, string> LoginParameters => null;
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (UseMockDataStore)
+                DependencyService.Register<MockDataStore>();
+            else
+                DependencyService.Register<CloudDataStore>();
+
+            SetMainPage();
+        }
+
+        public static void SetMainPage()
+        {
+            if (!UseMockDataStore && !Settings.IsLoggedIn)
+            {
+                Current.MainPage = new NavigationPage(new LoginPage())
+                {
+                    BarBackgroundColor = (Color)Current.Resources["Primary"],
+                    BarTextColor = Color.White
+                };
+            }
+            else
+            {
+                GoToMainPage();
+            }
+        }
+
+        public static void GoToMainPage()
+        {
+            Current.MainPage = new TabbedPage
+            {
+                Children = {
+                    new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Browse",
+                        Icon = Device.OnPlatform("tab_feed.png", null, null)
+                    },
+                    new NavigationPage(new AboutPage())
+                    {
+                        Title = "About",
+                        Icon = Device.OnPlatform("tab_about.png", null, null)
+                    },
+                }
+            };
+        }
+    }
+}
