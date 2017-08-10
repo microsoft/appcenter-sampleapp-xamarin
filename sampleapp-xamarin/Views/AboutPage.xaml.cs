d57145a70be2b0d78b642a01600fefd71dd66d1c
sabrinaj206 <sabrinajiang@Sabrinas-MacBook-Pro.local>
2017-08-10 14:40:47 -0700

    added project to git

---
+++ sampleapp-xamarin/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace sampleapp-xamarin
+{
+	public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+}
+}
---
+++ sampleapp-xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace sampleapp-xamarin
+{
+	public partial class ItemDetailPage : ContentPage
+{
+    ItemDetailViewModel viewModel;
+
+    public ItemDetailPage(ItemDetailViewModel viewModel)
+    {
+        InitializeComponent();
+
+        BindingContext = this.viewModel = viewModel;
+    }
+}
+}
