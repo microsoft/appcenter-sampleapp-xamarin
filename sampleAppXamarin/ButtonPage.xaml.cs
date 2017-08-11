10dead295805ea4acd9c7d3942810e71cff4944c
Sherin Shibu <sherin@Sherins-MacBook-Pro.local>
2017-08-11 12:06:47 -0700

    resolving merge conflicts

--- sampleAppXamarin/ButtonPage.xaml.cs
+++ sampleAppXamarin/ButtonPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
@@ -12,7 +12,6 @@ namespace sampleAppXamarin
             InitializeComponent();
         }
 
-<<<<<<< HEAD
 		public void Crash_Clicked(object sender, EventArgs e)
 		{
             DisplayAlert(null, "Sample crash sent.", "OK");
@@ -27,24 +26,5 @@ namespace sampleAppXamarin
 		{
             DisplayActionSheet("Send event with color property", "Cancel",null,"Purple","Yellow","Red","Blue");
 		}
-=======
-        public void Crash_Clicked(object sender, EventArgs e)
-        {
-            DisplayAlert("Send a sample crash", "Sample crash sent.", "OK");
-
-        }
-
-        public void Event_Clicked(object sender, EventArgs e)
-        {
-            DisplayAlert("Send a sample event", "Event sent.", "OK");
-
-        }
-
-        public void Color_Clicked(object sender, EventArgs e)
-        {
-            DisplayActionSheet("Send event with color property", "Cancel", "OK", "Purple", "Yellow", "Red", "Blue");
-
-        }
->>>>>>> b1f730a772f6769554bd49a849e216e16a315750
     }
 }
