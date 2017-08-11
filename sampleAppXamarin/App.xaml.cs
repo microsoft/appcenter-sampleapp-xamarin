8dcb1afccb603e084284a5cd64f5c9d14f9aed95
sabrinaj206 <sabrinajiang@Sabrinas-MacBook-Pro.local>
2017-08-11 16:36:31 -0700

    added getting started page code

--- sampleAppXamarin/App.xaml.cs
+++ sampleAppXamarin/App.xaml.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections.Generic;
-
 using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace sampleAppXamarin
 {
     public partial class App : Application
     {
+        
         public App()
         {
+			MobileCenter.Start("android=56a4c38c-9003-489d-b04b-c2c547fc23d5;" +
+				   "uwp={Your UWP App secret here};" +
+				   "ios={Your iOS App secret here}",
+				   typeof(Analytics), typeof(Crashes));
             MainPage = new ButtonPage();
         }
     }
