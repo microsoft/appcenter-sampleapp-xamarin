66402f049ef3e2ffdd9db0f9c63b007f3a23cd34
Sherin Shibu <sherin@Sherins-MBP.guest.corp.microsoft.com>
2017-08-10 18:49:29 -0700

    added button functionality and alerts

--- sampleAppXamarin/ButtonPage.xaml.cs
+++ sampleAppXamarin/ButtonPage.xaml.cs
@@ -11,5 +11,23 @@ namespace sampleAppXamarin
         {
             InitializeComponent();
         }
+
+		public void Crash_Clicked(object sender, EventArgs e)
+		{
+			DisplayAlert("Send a sample crash", "Sample crash sent.", "OK");
+
+		}
+
+		public void Event_Clicked(object sender, EventArgs e)
+		{
+            DisplayAlert("Send a sample event", "Event sent.", "OK");
+
+		}
+
+		public void Color_Clicked(object sender, EventArgs e)
+		{
+            DisplayActionSheet("Send event with color property", "Cancel","OK","Purple","Yellow","Red","Blue");
+
+		}
     }
 }
