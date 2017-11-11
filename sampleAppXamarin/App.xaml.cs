5c8c6c4359c0489b8c188e8981a724a3beaeba1b
Alexander Chocron <alchocro@microsoft.com>
2017-11-10 16:24:44 -0800

    MobileCenter -> AppCenter

--- sampleAppXamarin/App.xaml.cs
+++ sampleAppXamarin/App.xaml.cs
@@ -1,17 +1,16 @@
 ﻿using System.Collections.Generic;
 using Xamarin.Forms;
-using Microsoft.Azure.Mobile;
-using Microsoft.Azure.Mobile.Analytics;
-using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace sampleAppXamarin
 {
     public partial class App : Application
     {
-
         public App()
         {
-            MobileCenter.Start("android=56a4c38c-9003-489d-b04b-c2c547fc23d5;" +
+            AppCenter.Start("android=56a4c38c-9003-489d-b04b-c2c547fc23d5;" +
                    "ios=b21e0878-63f9-4fd0-bcbf-94d1760c7f91;",
                    typeof(Analytics), typeof(Crashes));
             MainPage = new ButtonPage();
