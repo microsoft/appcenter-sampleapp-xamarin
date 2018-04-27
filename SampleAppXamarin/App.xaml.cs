f16b1cd395bce886e16a08b14f21506876da7e0c
Ivan Matkov <v-ivmatk@microsoft.com>
2018-04-27 17:09:27 +0300

    Update AppCenter and add UWP project

---
+++ SampleAppXamarin/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace SampleAppXamarin
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new ButtonPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=<APP SECRET HERE>;" +
+                            "ios=<APP SECRET HERE>;" +
+                            "uwp=<APP SECRET HERE>;",
+                typeof(Analytics), typeof(Crashes));
+        }
+    }
+}
