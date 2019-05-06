7612364798c1fc8331d842310b3a758b110f5e48
Ilaiyaraja <yoursilaiyar@gmail.com>
2019-05-06 11:53:15 +0530

    Zip and unzip

--- Droid/appcenter-post-clone.sh
+++ Droid/appcenter-post-clone.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 echo "appcenter-post-clone.sh execution started"
-echo $APPCENTER_BRANCH+$APPCENTER_BUILD_ID.zip $APPCENTER_SOURCE_DIRECTORY
+echo "Source path="+$APPCENTER_BRANCH+$APPCENTER_BUILD_ID.zip $APPCENTER_SOURCE_DIRECTORY
+echo "Zipping started"
+zip -r $APPCENTER_BRANCH+$APPCENTER_BUILD_ID.zip $APPCENTER_SOURCE_DIRECTORY
+echo "Zipped to " $APPCENTER_SOURCE_DIRECTORY+$APPCENTER_BRANCH+$APPCENTER_BUILD_ID.zip $APPCENTER_SOURCE_DIRECTORY
+echo "Listing all files inside zip"
+unzip -l $APPCENTER_SOURCE_DIRECTORY+$APPCENTER_BRANCH+$APPCENTER_BUILD_ID.zip $APPCENTER_SOURCE_DIRECTORY | sed '1,3d;$d' | sed '$d'
 echo "appcenter-post-clone.sh done"
