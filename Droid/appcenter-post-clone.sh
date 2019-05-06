6d52dc6bf6b49be819b7b46c8c110c448393a714
Ilaiyaraja <yoursilaiyar@gmail.com>
2019-05-06 11:49:28 +0530

    Variable $ added to $APPCENTER_BUILD_ID

--- Droid/appcenter-post-clone.sh
+++ Droid/appcenter-post-clone.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "appcenter-post-clone.sh execution started"
-echo $APPCENTER_BRANCH+APPCENTER_BUILD_ID.zip $APPCENTER_SOURCE_DIRECTORY
+echo $APPCENTER_BRANCH+$APPCENTER_BUILD_ID.zip $APPCENTER_SOURCE_DIRECTORY
 echo "appcenter-post-clone.sh done"
