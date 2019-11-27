06c75805bc12f646805d0c26b163747100310dbe
Vyas Nellutla <vyasknellutla@gmail.com>
2019-11-27 08:50:18 -0600

    cleanup imports & updated dependency

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,11 +1,6 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace sampleAppXamarin.Droid
--- Droid/SplashActivity.cs
+++ Droid/SplashActivity.cs
@@ -1,15 +1,5 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 namespace sampleAppXamarin.Droid
 {
